3132e18760346b2d0788665c7d49646b3f364638
Daniil <https://github.com/DanyZy>
2018-10-19 16:51:48 +0600

    P4T1 fix

--- Practice4Task1.py
+++ Practice4Task1.py
@@ -1,6 +1,13 @@
 import unittest
 
 
+def add_spaces(text_w_space, text_wo_space, alphabet='abcdefghijklmnopqrstuvwxyz'):
+    for i, el in enumerate(text_w_space):
+        if not alphabet.__contains__(el):
+            text_wo_space.insert(i, el)
+    return text_wo_space
+
+
 def del_surplus_symb(password, len_pas):
     for i in range(len_pas):
         del password[-1]
@@ -9,29 +16,32 @@ def del_surplus_symb(password, len_pas):
 
 def encrypt(open_text, password, alphabet='abcdefghijklmnopqrstuvwxyz'):
     alphabet = list(alphabet)
+    text = open_text
     open_text = open_text.split(' ')
     open_text = ''.join(open_text)
     open_text = list(open_text)
-    password = del_surplus_symb(list(password) + open_text, len(open_text))
+    password = del_surplus_symb(list(password) + open_text, len(password))
     i = 0
     for el in open_text:
         open_text[i] = alphabet[(alphabet.index(el) + alphabet.index(password[i])) % len(alphabet)]
         i += 1
+    open_text = add_spaces(text, open_text, alphabet)
     return ''.join(open_text)
 
 
 def decrypt(ciphered_text, password, alphabet='abcdefghijklmnopqrstuvwxyz'):
     alphabet = list(alphabet)
+    text = ciphered_text
     ciphered_text = ciphered_text.split(' ')
     ciphered_text = ''.join(ciphered_text)
     ciphered_text = list(ciphered_text)
+    len_pass = len(password)
     if len(ciphered_text) > len(password):
         password = list(password)
         j = 0
         for el in ciphered_text:
-            if j < len(ciphered_text) - len(password):
+            if j < len(ciphered_text) - len_pass:
                 if alphabet.__contains__(el):
-                    # добавлять не зашифрованный хвост к ключу, а просто кучок открытого сообщения
                     password.append(alphabet[(alphabet.index(el) - alphabet.index(password[j])) % len(alphabet)])
                     j += 1
     i = 0
@@ -39,6 +49,7 @@ def decrypt(ciphered_text, password, alphabet='abcdefghijklmnopqrstuvwxyz'):
         if alphabet.__contains__(el):
             ciphered_text[i] = alphabet[(alphabet.index(el) - alphabet.index(password[i])) % len(alphabet)]
             i += 1
+    ciphered_text = add_spaces(text, ciphered_text, alphabet)
     return ''.join(ciphered_text)
 
 
@@ -54,6 +65,7 @@ class TestVigener(unittest.TestCase):
         text = 'codewars'
         pwd = 'password'
         cipher = 'rovwsoiv'
+        self.assertEqual(decrypt(cipher, pwd), text)
         self.assertEqual(encrypt(text, pwd), cipher)
 
     def test_two(self):
@@ -61,8 +73,9 @@ class TestVigener(unittest.TestCase):
         pwd = 'password'
         cipher = 'laxxhsj'
         self.assertEqual(decrypt(cipher, pwd), text)
+        self.assertEqual(encrypt(text, pwd), cipher)
 
-    def text_third(self):
+    def test_third(self):
         text = 'amazingly few discotheques provide jukeboxes'
         pwd = 'password'
         cipher = 'pmsrebxoy rev lvynmylatcwu dkvzyxi bjbswwaib'
